f29dc03d886735277e5a08b40f7c9709a8998118
BOI1000 <97549749+BOI1000@users.noreply.github.com>
2025-12-26 22:25:40 -0700

    created enumeration/nmap-default-scripts.sh created file_scripts/append-etc-hosts.sh

---
+++ file_scripts/append-etc-hosts.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+if [ $UID -ne 0 ]; then
+    echo "[-] This script must be run as root"
+    exit 1
+fi
+
+hosts_file="/etc/hosts"
+entry="$1"
+hostnames="${@:2}"
+
+if [ -z "$entry" ] || [ -z "$hostnames" ]; then
+    echo "Usage: $0 <IP_ADDRESS> <HOSTNAME1> [HOSTNAME2 ... HOSTNAMEN]"
+    exit 1
+fi
+
+if ! [[ "$entry" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    echo "[-] Invalid IP address: $entry"
+    echo "Usage: $0 <IP_ADDRESS> <HOSTNAME1> [HOSTNAME2 ... HOSTNAMEN]"
+    exit 1
+fi
+
+for hostname in $hostnames; do
+    if grep -qE "^\s*$entry\s+$hostname(\s|$)" "$hosts_file"; then
+        echo "[*] Entry for $hostname, IP: $entry already exists in $hosts_file"
+    else
+        echo "[*] Adding entry: $entry $hostname to $hosts_file"
+        echo -e "$entry\t$hostname" >> "$hosts_file"
+    fi
+done
+
+echo "[*] Completed updating $hosts_file"
